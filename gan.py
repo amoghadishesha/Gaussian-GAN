caa74c620d7bad2b2b55629e4b479e15747410c1
Amogh Adishesha <adisheshaamogh@gmail.com>
2017-10-16 16:57:28 -0400

    Update gan.py

--- gan.py
+++ gan.py
@@ -1,16 +1,3 @@
-'''
-An example of distribution approximation using Generative Adversarial Networks
-in TensorFlow.
-
-Based on the blog post by Eric Jang:
-http://blog.evjang.com/2016/06/generative-adversarial-nets-in.html,
-
-and of course the original GAN paper by Ian Goodfellow et. al.:
-https://arxiv.org/abs/1406.2661.
-
-The minibatch discrimination technique is taken from Tim Salimans et. al.:
-https://arxiv.org/abs/1606.03498.
-'''
 
 import argparse
 import numpy as np
